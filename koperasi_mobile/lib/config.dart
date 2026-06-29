840e3140269790a80a36bcdaf6b003bce13adc94
djgiffari <dj.giffari@gmail.com>
2026-06-29 19:14:51 +0700

    chore: make api configs fully automated for local and production

--- koperasi_mobile/lib/config.dart
+++ koperasi_mobile/lib/config.dart
@@ -2,8 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'dart:io';
 
 class ApiConfig {
-  // Set to false if you want to test on local server (localhost)
-  static const bool isProduction = true;
+  // Secara otomatis mendeteksi apakah aplikasi sedang di-build (Release) atau berjalan di emulator/debug
+  static const bool isProduction = kReleaseMode;
 
   static String get baseUrl {
     if (isProduction) {
@@ -20,6 +20,7 @@ class ApiConfig {
       return 'http://localhost:3000/api';
     }
     // If running on Android emulator, use 10.0.2.2
+    // CATATAN: Jika menggunakan HP fisik via kabel USB, ganti 10.0.2.2 dengan IP WiFi komputer Anda (misal 192.168.1.5)
     return 'http://10.0.2.2:3000/api';
   }
 }
